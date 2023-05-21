b5d37de99f3fa62097b8ebed03d7a531f2931dc0
josephinecowley <josephinecowley1010@gmail.com>
2023-05-21 19:02:32 +0100

    feat: implement skeleton of parser and some development of the connection_list and connection functions

--- logsim/logsim/parse.py
+++ logsim/logsim/parse.py
@@ -8,6 +8,9 @@ Classes
 -------
 Parser - parses the definition file and builds the logic network.
 """
+from names import Names
+# Will need to add more imports
+from scanner import Scanner
 
 
 class Parser:
@@ -35,10 +38,87 @@ class Parser:
 
     def __init__(self, names, devices, network, monitors, scanner):
         """Initialise constants."""
+        names = Names()
+
+    def device_list(self):
+        """Parse device list"""
+        DEVICES_ID = self.names.lookup(["DEVICES"])
+        if (self.symbol.type == self.scanner.KEYWORD and self.symbol.id == DEVICES_ID):
+            self.symbol = self.scanner.get_symbol()
+            if (self.symbol.type == self.scanner.BRACE_OPEN):
+                self.symbol = self.scanner.get_symbol()
+            else:
+                error("expected an open brace")
+
+        else:
+            error("expected \"DEVICES\" as keyword")
+        pass
+
+    def device():
+        pass
+
+    def connection_list():
+        """Parse connection list"""
+        CONNECTIONS_ID = self.names.lookup(["CONNECTIONS"])
+        # Check first symbol is "CONNECTIONS"
+        if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == CONNECTIONS_ID:
+            self.symbol = self.scanner.get_symbol()
+            # Check next symbol is an open brace "{"
+            if self.symbol.type == self.scanner.BRACE_OPEN:
+                self.symbol = self.scanner.get_symbol()
+                self.connection()
+                # Repeat checking connections in list until the close brace "}"
+                while self.symbol.type == self.scanner.SEMICOLON and != self.scanner.BRACE_CLOSE:
+                    self.symbol = self.scanner.get_symbol()
+                    self.connection()
+                if self.symbol.type == self.scanner.BRACE_CLOSE:
+                    self.symbol = self.scanner.get_symbol()
+                else:
+                    error("Expected a } after connections list")
+            else:
+                error("Expected a { after CONNECTIONS")
+        else:
+            error("expected \"CONNECTIONS\" keyword")
+
+    def connection():
+        """Parse a single connection line"""
+        self.output()
+        # Check ouput connection is followed by an equals sign "="
+        if self.symbol.type == self.scanner.EQUALS:
+            self.symbol = self.scanner.getsymbol()
+            self.input()
+        else:
+            error("Need to follow the output connection with an equals")
+
+    def output():
+        """Parse a single device output"""
+
+    def input():
+        """Parse a single device output"""
+        pass
+
+    def monitor_list():
+        pass
+
+    def error(string):
+        print(string)
 
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
+        self.symbol = self.scanner.getsymbol()
+        # Check to see if file is empty
+        # if file empty: throw error
+
+        # Parse device list
+        self.device_list()
+
+        # Parse connection list
+        self.connection_list()
+
+        # Parse monnitor list
+        self.monitor_list()
+
         return True
