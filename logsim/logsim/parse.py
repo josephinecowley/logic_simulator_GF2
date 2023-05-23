9470c6c4291f650de42c008905ece2a4a3e582c3
josephinecowley <josephinecowley1010@gmail.com>
2023-05-23 13:00:02 +0100

    feat: add syntax skeleton for monitor and end functions in parser. add functionality for testing with names and scanner

--- logsim/logsim/parse.py
+++ logsim/logsim/parse.py
@@ -9,7 +9,7 @@ Classes
 Parser - parses the definition file and builds the logic network.
 """
 from names import Names
-# Will need to add more imports
+# Will need to add more imports once building network
 from scanner import Scanner
 
 
@@ -36,14 +36,15 @@ class Parser:
     parse_network(self): Parses the circuit definition file.
     """
 
-    def __init__(self, names, devices, network, monitors, scanner):
+#    def __init__(self, names, devices, network, monitors, scanner):
+    def __init__(self, names, scanner):
         """Initialise constants."""
         self.names = names
         self.scanner = scanner
 
     def device_list(self):
         """Parse device list"""
-        DEVICES_ID = self.names.lookup(["DEVICES"])
+        DEVICES_ID = self.names.lookup(["DEVICES"])[0]
         # Check first entry in file is DEVICES
         if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == DEVICES_ID:
             self.symbol = self.scanner.get_symbol()
@@ -54,19 +55,18 @@ class Parser:
                 # Check all devices in list, which are all separated by semicolons
                 while ((self.symbol.type == self.scanner.SEMICOLON) and (self.symbol.type != self.scanner.BRACE_CLOSE)):
                     self.symbol = self.scanner.get_symbol()
-                    self.devce()
+                    self.device()
                 # Check for the end of file symbol "}"
                 if self.symbol.type == self.scanner.BRACE_CLOSE:
                     self.symbol = self.scanner.get_symbol()
                 else:
-                    error("Expected a } to close the devices list")
+                    print("Expected a } to close the devices list")
             else:
-                error("expected an open brace")
-
+                print("expected an open brace")
         else:
-            error("expected \"DEVICES\" as keyword")
+            print("expected \"DEVICES\" as keyword")
 
-    def device():
+    def device(self):
         """Parse user defined devices"""
         # Check that we have a valid user defined name
         if self.symbol.type == self.scanner.NAME:
@@ -78,13 +78,16 @@ class Parser:
                 if self.symbol.type == self.scanner.NAME:
                     symbol_ID, device_input = self.check_device_is_valid()
                     self.symbol = self.scanner.get_symbol()
+                else:
+                    print(
+                        "Expected a valid name specifying a device, starting with a letter")
             else:
-                error("Expected an equals sign after user name for device")
+                print("Expected an equals sign after user name for device")
         else:
-            error("Expected a user defined name.")
+            print("Expected a user defined name.")
 
-    def check_device_is_valid():
-        """Returns the device type and the input"""
+    def check_device_is_valid(self):
+        """Returns both device type ID and the input ID"""
         [AND_ID, NAND_ID, OR_ID, NOR_ID, XOR_ID, DTYPE_ID, SWITCH_ID, CLK_ID] = self.names.lookup(
             ["AND", "NAND", "OR", "NOR", "XOR", "DTYPE",  "SWITCH", "CLK"])
         one_to_sixteen = self.names.lookup([
@@ -101,22 +104,22 @@ class Parser:
                 if self.symbol.type == self.scanner.NUMBER:
                     # Check that number is within range
                     if self.symbol.id in one_to_sixteen:
-                        number_of_inputs = self.symbol
+                        number_of_inputs_ID = self.symbol.id
                         self.symbol = self.scanner.get_symbol()
                         # Check that next symbol is a close bracket ")"
                         if self.symbol.type == self.scanner.RIGHT_BRACKET:
-                            return symbol_ID, number_of_inputs
+                            return symbol_ID, number_of_inputs_ID
                         else:
-                            error("Expected a close bracket after input number")
+                            print("Expected a close bracket after input number")
                             return None, None
                     else:
-                        error("Expected an input number between 1 and 16 inclusive")
+                        print("Expected an input number between 1 and 16 inclusive")
                         return None, None
                 else:
-                    error("Expected a number as an input")
+                    print("Expected a number as an input")
                     return None, None
             else:
-                error("Expected bracket then an input for AND NAND NOR OR gates")
+                print("Expected bracket then an input for AND NAND NOR OR gates")
                 return None, None
         # Check if symbol is an XOR or DTYPE (with no inputs)
         elif self.symbol.id == XOR_ID or self.symbol.id == DTYPE_ID:
@@ -133,26 +136,51 @@ class Parser:
                 if self.symbol.type == self.scanner.NUMBER:
                     # Check that number is within range
                     if self.symbol.id in binary_digit:
-                        switch_initial_state = self.symbol
-                        return symbol_ID, switch_initial_state
+                        switch_initial_state = self.symbol.id
+                        return symbol_ID, switch_initial_state_ID
                     else:
-                        error(
+                        print(
                             "Expected a binary digit to specify initial switch state")
                         return None, None
                 else:
-                    error("Expected a number as an input")
+                    print("Expected a number as an input")
                     return None, None
             else:
-                error("Expected bracket then an input for AND NAND NOR OR gates")
+                print("Expected bracket then an input for AND NAND NOR OR gates")
                 return None, None
-
         # Check if symbol is a CLK
-
+        elif self.symbol.id == CLK_ID:
+            symbol_ID = self.symbol.id
+            self.symbol = self.scanner.get_symbol()
+            # Check that the gate is followed by an open bracket symbol
+            if self.symbol.type == self.scanner.LEFT_BRACKET:
+                self.symbol = self.scanner.get_symbol()
+                # Check that number of inputs is an integer
+                if self.symbol.type == self.scanner.NUMBER:
+                    number_of_cycles_ID = self.symbol.id
+                    number_of_cycles = int(
+                        self.names.get_name_string(self.symbol.id))
+                    # Check that input number is negative (this is a semantic check!)
+                    if number_of_cycles > 0:
+                        # Check that the next symbol is a close bracket
+                        self.symbol = self.scanner.get_symbol()
+                        if self.symbol.type == self.scanner.RIGHT_BRACKET:
+                            return symbol_ID, number_of_cycles_ID
+                    else:
+                        print("Expected a positive integer")
+                        return None, None
+                else:
+                    print(
+                        "Expected an integer for number of cycles of each clock half period")
+                    return None, None
+            else:
+                print("Expected bracket then an input for AND NAND NOR OR gates")
+                return None, None
         else:
-            error("Ensure your device name is one of the valid device names")
+            print("Ensure your device name is one of the valid device names")
             return None, None
 
-    def connection_list():
+    def connection_list(self):
         """Parse connection list"""
         CONNECTIONS_ID = self.names.lookup(["CONNECTIONS"])
         # Check first symbol is "CONNECTIONS"
@@ -169,23 +197,23 @@ class Parser:
                 if self.symbol.type == self.scanner.BRACE_CLOSE:
                     self.symbol = self.scanner.get_symbol()
                 else:
-                    error("Expected a } after connections list")
+                    print("Expected a } after connections list")
             else:
-                error("Expected a { after CONNECTIONS")
+                print("Expected a { after CONNECTIONS")
         else:
-            error("expected \"CONNECTIONS\" keyword")
+            print("expected \"CONNECTIONS\" keyword")
 
-    def connection():
+    def connection(self):
         """Parse a single connection line"""
         self.output()
         # Check ouput connection is followed by an equals sign "="
         if self.symbol.type == self.scanner.EQUALS:
-            self.symbol = self.scanner.getsymbol()
+            self.symbol = self.scanner.get_symbol()
             self.input()
         else:
             error("Need to follow the output connection with an equals")
 
-    def output():
+    def output(self):
         """Parse a single device output"""
         valid_dtype_output_id_list = self.names.lookup(["Q", "QBAR"])
         # Check that the output to be connected is a name
@@ -200,13 +228,13 @@ class Parser:
                     if self.symbol.id in valid_dtype_output_id_list:
                         self.symbol = self.scanner.get_symbol()
                     else:
-                        error("Expected name to be either Q or QBAR.")
+                        print("Expected name to be either Q or QBAR.")
                 else:
-                    error("Expected name after fullstop")
+                    print("Expected name after fullstop")
         else:
-            error("Expected a device name")
+            print("Expected a device name")
 
-    def input():
+    def input(self):
         """Parse a single device input"""
         valid_input_suffix_id_list = self.names.lookup(["I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9",
                                                         "I10", "I11", "I12", "I13", "I14", "I15", "I16", "DATA", "CLK", "SET", "CLEAR"])
@@ -222,40 +250,71 @@ class Parser:
                     if self.symbol.id in valid_input_suffix_id_list:
                         self.symbol = self.scanner.get_symbol()
                     else:
-                        error("Expected a valid input suffix")
+                        print("Expected a valid input suffix")
             else:
-                error("Expected a full stop followed by an input suffix")
+                print("Expected a full stop followed by an input suffix")
         else:
-            error("Expected a device name to specify the input for")
+            print("Expected a device name to specify the input for")
 
-    def monitor_list():
-        pass
+    def monitor_list(self):
+        """Parse monitor list"""
+        # Check that the first is a valid output name
+        self.output()
+        # Repeat checking connections in list until the close brace "}"
+        while ((self.symbol.type == self.scanner.SEMICOLON) and (self.symbol.type != self.scanner.BRACE_CLOSE)):
+            self.symbol = self.scanner.get_symbol()
+            self.output()
+        if self.symbol.type == self.scanner.BRACE_CLOSE:
+            self.symbol = self.scanner.get_symbol()
 
-    def error(string):
-        print(string)
+    def end(self):
+        """Parse an END symbol"""
+        # Check that the final symbol after the } is an END symbol
+        END_ID = self.names.lookup(["END"])[0]
+        if self.symbol.id == END_ID:
+            self.symbol = self.scanner.get_symbol()
+            if self.symbol.id == self.scanner.EOF:
+                # Will need to return error count, etc
+                return True
+            else:
+                print("Cannot have any text after END symbol")
+        else:
+            print("Expected END keyword to signify end of file")
 
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
-        self.symbol = self.scanner.getsymbol()
+        self.symbol = self.scanner.get_symbol()
         # Check to see if file is empty
-        # if file empty, throw an error
         if self.symbol.type == self.scanner.EOF:
-            error("Cannot parse an empty text file.")
+            print("Cannot parse an empty text file.")
         else:
             # Parse device list
             self.device_list()
-
             # Parse connection list
             self.connection_list()
 
             # Parse monnitor list
             self.monitor_list()
 
-            # Check for the END of file
-
-            # Return True if error count is zero, otherwise return false
-
+            # Check for END keyword
+            self.end()
         return True
+
+
+# Return True if error count is zero, otherwise return false
+
+
+def main():
+    # Check command line arguments
+    file_path = "logsim/example1_logic_description.txt"
+    names = Names()
+    scanner = Scanner(file_path, names)
+    parser = Parser(names, scanner)
+    print(parser.parse_network())
+
+
+if __name__ == "__main__":
+    main()
