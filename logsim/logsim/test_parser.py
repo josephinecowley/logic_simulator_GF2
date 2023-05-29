0e1d044d4ad789d104dad1668656e48a4d311f50
Khalid-Omar-77 <67450525+Khalid-Omar-77@users.noreply.github.com>
2023-05-29 09:38:14 +0100

    feat: add more examples to tests for check_device_is_valid

--- logsim/logsim/test_parser.py
+++ logsim/logsim/test_parser.py
@@ -523,7 +523,9 @@ def test_parser_check_device_is_valid_correct_example(parser_fixture, create_tes
 
 
 @pytest.mark.parametrize("example, expected", [
-    ("AND(12", "\n  Line 3: Syntax Error: Expected a ')' for an input\n \n"),
+    #("AND(12", "  Line 3: Syntax Error: Expected a ')' for an input\n"),
+    ("AND(34)", "  Line 2: Semantic Error: Input number of gates is out of range. Must be an integer between 1 and 16\n"),
+    ("AND()", "  Line 2: Syntax Error: Expected a positive integer"),
 ])
 def test_parser_check_device_is_valid_erroneous_examples(parser_fixture, create_testing_file_to_scan, capfd, example, expected):
     scanner = create_testing_file_to_scan(
@@ -533,11 +535,11 @@ def test_parser_check_device_is_valid_erroneous_examples(parser_fixture, create_
     )
     parser = parser_fixture(scanner)
     parser.symbol = parser.scanner.get_symbol()
-
+    breakpoint()
     parser.check_device_is_valid()
     
     captured = capfd.readouterr()
-    printed_message = captured.out
+    printed_message = captured.out.splitlines(True)[1]
 
     assert printed_message == expected
 
